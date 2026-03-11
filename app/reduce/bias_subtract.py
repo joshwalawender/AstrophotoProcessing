05bc03f9b3f8ce649a3bd2dd5f2b5b810f864628
Josh Walawender <joshwalawender@me.com>
2026-03-10 16:58:51 -1000

    Use floats to avoid wrapping uints. Drop ccdproc.

--- app/reduce/bias_subtract.py
+++ app/reduce/bias_subtract.py
@@ -1,5 +1,7 @@
 # from astropy.nddata import CCDData
-import ccdproc
+import numpy as np
+from astropy.nddata import CCDData
+# import ccdproc
 
 from app import log
 from app.data_models.OSCImage import OSCImage
@@ -13,8 +15,11 @@ def bias_subtract(DM, master_bias=None):
     assert isinstance(master_bias, OSCImage)
 
     log.info('Subtracting bias')
-    bias_subtracted = ccdproc.subtract_bias(DM.data, master_bias.data)
-#     bias_subtracted = DM.data.data - master_bias.data.data
+    # Need to force data in to float to avoid uint wrapping for negative values
+    zeros = np.zeros(DM.data.shape, dtype=float)
+    image = DM.data.subtract(CCDData(zeros, unit='adu'))
+#     bias_subtracted = ccdproc.subtract_bias(image, master_bias.data)
+    bias_subtracted = image.data - master_bias.data
     DM.update_data(bias_subtracted,
                    header=[('BIASSUB', True, 'Bias subtracted')],
                    history=['Bias subtracted'])
