13ffcf5c4f6b9282e82897aab69ecb20b947166f
Josh Walawender <jmwalawender@gmail.com>
2026-03-21 21:02:14 -1000

    Drop multiprocessings for now

--- app/reduce/reproject.py
+++ app/reduce/reproject.py
@@ -1,3 +1,4 @@
+# from multiprocessing import Process
 import numpy as np
 from astropy.wcs import WCS
 from ccdproc import wcs_project
@@ -10,9 +11,21 @@ from app.data_models.OSCImage import OSCImage
 ## 
 ##-------------------------------------------------------------------------
 def reproject(DM, reference_wcs=None):
-    assert isinstance(DM, OSCImage)
-    assert isinstance(reference_wcs, WCS)
+    if not isinstance(DM, OSCImage):
+        log.error(f'Input data model is {type(DM)}')
+    if not isinstance(reference_wcs, WCS):
+        log.error(f'Input WCS is {type(DM)}')
     DM.split_colors()
+    log.info(f'Reprojecting color components')
+#     pR = Process(target=wcs_project, args=(DM.red, reference_wcs))
+#     pG = Process(target=wcs_project, args=(DM.green, reference_wcs))
+#     pB = Process(target=wcs_project, args=(DM.blue, reference_wcs))
+#     pR.start()
+#     pG.start()
+#     pB.start()
+#     pR.join()
+#     pG.join()
+#     pB.join()
 
     log.debug('Reprojecting red image')
     DM.red = wcs_project(DM.red, reference_wcs)
